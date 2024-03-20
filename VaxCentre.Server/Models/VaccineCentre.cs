ecc1432b69fc3a064161e861b6422efbb9433a07
Abdelrahman <abdelrahmanheshamswe25@gmail.com>
2024-03-20 23:07:40 +0200

    F3 variables defined

---
+++ VaxCentre.Server/Models/VaccineCentre.cs
@@ -0,0 +1,11 @@
+﻿namespace VaxCentre.Server.Models
+{
+    public class VaccineCentre
+    {
+        public int ID { get; set; }
+        public required string displayName { get; set; }
+        public required string username { get; set; }
+        public required string password { get; set; }
+        public required string address { get; set; }
+    }
+}
