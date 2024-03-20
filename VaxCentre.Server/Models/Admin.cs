4c57479a0e819fdee7a3d0d8343e5311dd3a9413
Abdelrahman <abdelrahmanheshamswe25@gmail.com>
2024-03-20 22:42:44 +0200

    F1 variables written

---
+++ VaxCentre.Server/Models/Admin.cs
@@ -0,0 +1,9 @@
+﻿namespace VaxCentre.Server.Models
+{
+    public class Admin
+    {
+        public required int ID { get; set; }
+        public required string username { get; set; }
+        public required string password { get; set; }
+    }
+}
