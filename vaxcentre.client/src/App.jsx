c0f6e37f57123d010dab8fd10016dcc38db5d6d1
Abdelrahman151515 <abdoyassin000@gmail.com>
2024-05-10 01:56:26 +0300

    vaccine reservation for each center (F-22)

--- vaxcentre.client/src/App.jsx
+++ vaxcentre.client/src/App.jsx
@@ -3,11 +3,13 @@ import './App.css';
 import SignupForm from './Pages/Signup-form/Signup-form';
 import SigninForm from './Pages/Signin-form/Signin-form';
 import ListUsers from './Pages/Admin-Centers/Accept-Reject-Users';
+import Patient from './Pages/Patient-Centers/Paitent-Centers';
+
 function App() {
     return (
         <>
-         
-            <ListUsers />
+            
+            <Patient />
         </>
     );
 }
